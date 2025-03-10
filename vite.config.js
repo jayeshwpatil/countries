f2a782de8b42b15fd573f8164ddcb459e4bbb916
jayeshwpatil <jayeshwpatil@gmail.com>
2025-03-10 16:39:16 +0530

    vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/countries/', //name of git repository
   plugins: [
     vue(),
     vueDevTools(),
